2228f68ce0e3fcbf2dcfa28f315cf2ee35e14362
Aizen741 <rahulsivadas2009@gmail.com>
2019-10-26 06:40:52 +0530

    #6 linkedEmail

--- Website.py
+++ Website.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
 
 URL = 'https://www.amazon.in/Apple-iPhone-6S-Space-Storage/dp/B01LX3A7CC/ref=sr_1_1?keywords=apple&qid=1572036660&smid=A14CZOWI0VEHLG&sr=8-1'
@@ -9,14 +10,48 @@ headers = {
     "User-Agents": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
            }
 
-page = requests.get(URL, headers = headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-print(soup.prettify())
+def check_price():
+    page = requests.get(URL, headers = headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    print(soup.prettify())
 
-title = soup.find(id= "productTitle").get_text()
-price = soup.find(id="priceblock_dealprice").get_text()
-converted_price = float(price[0:10])
+    title = soup.find(id= "productTitle").get_text()
+    price = soup.find(id="priceblock_dealprice").get_text()
+    converted_price = float(price[0:10])
 
-print(converted_price)
-print(title.strip())
+    if(converted_price < 20000):
+        send_mail()
+
+    print(converted_price)
+    print(title.strip())
+
+    if(converted_price > 22000):
+        send_mail()
+
+
+
+def send_mail():
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
+
+
+    server.login('das81573@gmail.com','areyouready2009')
+
+    subject = "The Price is low now"
+    body = "Check the link : 'https://www.amazon.in/Apple-iPhone-6S-Space-Storage/dp/B01LX3A7CC/ref=sr_1_1?keywords=apple&qid=1572036660&smid=A14CZOWI0VEHLG&sr=8-1'"
+
+    msg = f"Subject: {subject}\n\n{body}"
+    server.sendmail(
+        'das81573@gmail.com',
+        'rahulsivadas2009@gmail.com',
+        msg
+    )
+    print("Hey Email has been send")
+
+    server.quit()
+
+
+check_price()
